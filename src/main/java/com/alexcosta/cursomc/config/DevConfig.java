19cd97f092c3950ecc8a1cd9b0b3bd5da44e4a56
Alexandre Costa <alex.o.costa@gmail.com>
2020-07-13 16:44:33 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/alexcosta/cursomc/config/DevConfig.java
+++ src/main/java/com/alexcosta/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.alexcosta.cursomc.services.DBService;
+import com.alexcosta.cursomc.services.EmailService;
+import com.alexcosta.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 		
 		dbService.instatiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
