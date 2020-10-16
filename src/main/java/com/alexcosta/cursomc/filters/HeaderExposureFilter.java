fea9c9101d4a15b40230d3d86e1dd63e427e749b
Alexandre Costa <alex.o.costa@gmail.com>
2020-10-16 20:00:39 -0300

    Expondo o header location nas respostas

---
+++ src/main/java/com/alexcosta/cursomc/filters/HeaderExposureFilter.java
@@ -0,0 +1,27 @@
+package com.alexcosta.cursomc.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeaderExposureFilter implements Filter {
+
+	// Filtro para interceptar todas as requisições e expor o header location (para que o frontend angular consiga acessá-lo)
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("access-control-expose-headers", "location");
+		chain.doFilter(request, response);
+	}
+
+}
